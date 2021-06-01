4d1b4513b748648d0772c28a58be87654bfcb18b
dredddddd <85181241+dredddddd@users.noreply.github.com>
2021-06-01 18:14:10 +0300

    Add files via upload

---
+++ Triangle.h
@@ -0,0 +1,29 @@
+"Kudryashov-211"
+#pragma once
+#include "Vector.h"
+
+class Triangle
+{
+private:
+	Vector A, B, C;
+	int id;
+	int neighbouring_triangle_id;
+public:
+	Triangle();
+	Triangle(Vector a, Vector b, Vector c);
+	Triangle(const Triangle& T);
+	~Triangle() = default;
+	const Triangle& operator=(const Triangle& T);
+	Vector get_A();
+	Vector get_B();
+	Vector get_C();
+	int get_id();
+	int get_neighbouring_triangle_id();
+	void assign_A(Vector a);
+	void assign_B(Vector b);
+	void assign_C(Vector c);
+	void assign_id(int k);
+	void assing_neighbouring_triangle_id(int k);
+	void print_triangle(ofstream& file);
+};
+
